2895bc904d63784384e1076b6147fcfe6f90cbfc
Swenchao <scwrite678@outlook.com>
2022-03-02 11:30:07 +0800

    打成war包

--- src/main/java/com/sc/BlogApplication.java
+++ src/main/java/com/sc/BlogApplication.java
@@ -2,10 +2,30 @@ package com.sc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.domain.Specification;
 
+//@SpringBootApplication
+//public class BlogApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(BlogApplication.class, args);
+//    }
+//
+//}
+
+/**
+ * @author swenchao
+ * 打war包，使用外置tomcat部署
+ */
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlogApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
